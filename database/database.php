7fd6b5260fa2b47faa357c7cfbaa92e2a15c198a
johnmarkvictoria23 <juanmarkotumanan@gmail.com>
2025-03-26 14:55:20 +0800

    Update database.php

--- database/database.php
+++ database/database.php
@@ -3,3 +3,17 @@ $servername = "localhost";
 $db_name = "group_ten_midproject";
 $username = "root";
 $password = "";
+
+// Create connection
+$conn = new mysqli($servername,$username,$password,$db_name);
+
+// Check connection
+if($conn->connect_error) {
+    die("Connection Failed: " . $conn->connect_error);
+}else{
+    die("Connection Succesfull");
+}
+
+
+?>
+
