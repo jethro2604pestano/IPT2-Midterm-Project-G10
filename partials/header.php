2665104c2417c72e4e36af52cba243914e09a1ba
Jamess101 <jamesphillipguatno7@gmail.com>
2025-03-26 16:46:52 +0800

    Update header.php

--- partials/header.php
+++ partials/header.php
@@ -71,12 +71,12 @@
 
           <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
             <img src="assets/img/profile-img.jpg" alt="Profile" class="rounded-circle">
-            <span class="d-none d-md-block dropdown-toggle ps-2">J. Dela Cruz</span>
+            <span class="d-none d-md-block dropdown-toggle ps-2">J. Pestaño</span>
           </a><!-- End Profile Iamge Icon -->
 
           <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
             <li class="dropdown-header">
-              <h6>Juan Dela Cruz</h6>
+              <h6>Jethro Pestaño</h6>
               <span>Web Designer</span>
             </li>
             <li>
